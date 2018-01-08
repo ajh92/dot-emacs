4e8127776def72321aa266bf1fbc1b722bebf629
Andrew Hill <andrew@andrewhill.us>
2018-01-08 09:41:52 -0500

    Use-package for js2-mode

--- config/ajh-language-config.el
+++ config/ajh-language-config.el
@@ -28,7 +28,9 @@
 
 
 ;;; Javascript
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(use-package js2-mode
+  :ensure t
+  :config (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
 
 
 ;;; Markdown
